1f22ce990c4286ebee5bf4ab3b7e3132d1d2668c
Matthew Preucil <mattpsbs@gmail.com>
2017-07-21 00:14:47 -0700

    Add Travis.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "BigMath",
     dependencies: [
-        .Package(url: "https://github.com/makecph/CMPFR", majorVersion: 1)
+        .Package(url: "https://github.com/randymarsh77/CMPFR", majorVersion: 1)
     ]
 )
