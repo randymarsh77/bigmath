cd1c2cb7b8ecd6434efa4e938db6e90f3b837bab
Matthew Preucil <mattpsbs@gmail.com>
2018-01-14 22:05:12 -0800

    Expand precision when powering up.
    - Also needed everywhere else.

--- Sources/BigMath/BigFloat/bigfloat+pow.swift
+++ Sources/BigMath/BigFloat/bigfloat+pow.swift
@@ -3,7 +3,7 @@ import CMPFR
 
 public func pow(_ x: BigFloat, _ e: BigFloat) -> BigFloat {
 	var data = mpfr_t()
-	mpfr_init(&data)
+	mpfr_init2(&data, x._data._mpfr_prec * e._data._mpfr_prec)
 	var inp = x._data
 	var exp = e._data
 	mpfr_pow(&data, &inp, &exp, MPFR_RNDN)
