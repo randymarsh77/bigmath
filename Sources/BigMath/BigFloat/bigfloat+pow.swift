1240622162c87f69c7361253e4223661577701b1
Matthew Preucil <mattpsbs@gmail.com>
2018-01-23 21:15:19 -0800

    Fix pow(x, 0).

--- Sources/BigMath/BigFloat/bigfloat+pow.swift
+++ Sources/BigMath/BigFloat/bigfloat+pow.swift
@@ -3,7 +3,7 @@ import CMPFR
 
 public func pow(_ x: BigFloat, _ e: BigFloat) -> BigFloat {
 	var data = mpfr_t()
-	mpfr_init2(&data, x._data._mpfr_exp * e.toInt())
+	mpfr_init2(&data, x._data._mpfr_exp * max(1, e.toInt()))
 	var inp = x._data
 	var exp = e._data
 	mpfr_pow(&data, &inp, &exp, MPFR_RNDN)
--- Tests/BigMathTests/bigfloat+pow.tests.swift
+++ Tests/BigMathTests/bigfloat+pow.tests.swift
@@ -13,4 +13,15 @@ class BigFloatPowTests: XCTestCase
 		}
 		XCTAssert(v == c)
 	}
+
+	func testPowTo0() {
+		let a: BigFloat = 10
+		let b: BigFloat = 0
+		let c: BigFloat = 1
+		var v: BigFloat = 0
+		self.measure {
+			v = pow(a, b)
+		}
+		XCTAssert(v == c)
+	}
 }
